f31b015d51ee51d01af5c38bd5f49678d38398ab
ehmusman <ehmusman@gmail.com>
2020-12-13 19:03:43 +0500

    not found page is added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 const App = () => {
 
@@ -28,6 +29,7 @@ const App = () => {
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User}
                 />
+                <Route component={NotFound} />
               </Switch>
 
             </div>
