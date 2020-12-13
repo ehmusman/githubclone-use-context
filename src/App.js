610dbca68e88c783cc6d50da39d3acc0cea653b9
ehmusman <ehmusman@gmail.com>
2020-12-13 18:59:58 +0500

    home page is added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/layout/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
 
 const App = () => {
 
@@ -23,12 +22,9 @@ const App = () => {
               <Alert
               />
               <Switch>
-                <Route exact path='/' render={props =>
-                  <React.Fragment>
-                    <Search />
-                    <Users />
-                  </React.Fragment>
-                } />
+                <Route exact path='/'
+                  component={Home}
+                />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User}
                 />
